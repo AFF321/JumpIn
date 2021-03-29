2b88789c66ee0b0e413481b8018d8d1511a3ac13
Chelsey Morris <chelseymorris91@yahoo.com>
2021-03-29 12:40:02 -0700

    m

--- public/js/create.js
+++ public/js/create.js
@@ -1,20 +1,31 @@
 $(document).ready(function () {
 
-    let eventName = $('#inputEventName');
-    let hostName = $('#inputHostName');
-    let eventDate = $('#eventDate');
-    let eventType = $('#inputEventType');
-    let eventDescr = $('#eventDescription');
-    let localeName = $('#inputLocationName');
-    let address = $('#inputStreetAddress');
-    let eventCity = $('#inputCity');
-    let eventState = $('#inputState');
-    let eventZip = $('#inputZip');
-    let inviteEmails = $('#inputEmails');
+    let eventName = $('#inputEventName').val();
+    let hostName = $('#inputHostName').val();
+    let eventDate = $('#eventDate').val();
+    let eventType = $('#inputEventType').val();
+    let eventDescr = $('#eventDescription').val();
+    let localeName = $('#inputLocationName').val();
+    let address = $('#inputStreetAddress').val();
+    let eventCity = $('#inputCity').val();
+    let eventState = $('#inputState').val();
+    let eventZip = $('#inputZip').val();
+    let inviteEmails = $('#inputEmails').val();
 
- 
+    const createFormHandle = async (event) => {
+        event.preventDefault();
 
+        const response = await fetch('/api/event', {
+            method: 'POST',
+            body: JSON.stringify({ eventName, hostName, eventDate,eventType, eventDescr, localeName, address, eventCity, eventState, eventZip, inviteEmails}),
+            headers: { 'Content-Type': 'application/json' },
+          });
+      
+          if (response.ok) {
+            alert('Your Event has been created!')
+          } 
+    }
 
 })
 
-
+$('submit').on("click", function() { createFormHandler(); });
