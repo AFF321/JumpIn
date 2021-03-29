c1793fc0bdb610c5001b47dd5eb7216c82ba49a6
Chelsey Morris <chelseymorris91@yahoo.com>
2021-03-29 10:42:06 -0700

    work

--- public/js/create.js
+++ public/js/create.js
@@ -1,11 +1,20 @@
-let eventName = ('#inputEventName');
-let hostName = ('#inputHostName');
-let eventDate = ('#eventDate');
-let eventType = ('#inputEventType');
-let eventDescr = ('#eventDescription');
-let localeName = ('#inputLocationName');
-let address = ('#inputStreetAddress');
-let eventCity = ('#inputCity');
-let eventState = ('#inputState');
-let eventZip = ('#inputZip');
-let inviteEmails = ('#inputEmails');
+$(document).ready(function () {
+
+    let eventName = $('#inputEventName');
+    let hostName = $('#inputHostName');
+    let eventDate = $('#eventDate');
+    let eventType = $('#inputEventType');
+    let eventDescr = $('#eventDescription');
+    let localeName = $('#inputLocationName');
+    let address = $('#inputStreetAddress');
+    let eventCity = $('#inputCity');
+    let eventState = $('#inputState');
+    let eventZip = $('#inputZip');
+    let inviteEmails = $('#inputEmails');
+
+
+
+    
+})
+
+
