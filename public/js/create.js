d37953cc15b4925c7e089bee16dd48b8eb6ff559
Chelsey Morris <chelseymorris91@yahoo.com>
2021-03-29 16:20:48 -0700

    fixed form

--- public/js/create.js
+++ public/js/create.js
@@ -1,26 +1,22 @@
 
-    const eventName = document.querySelector('#inputEventName').value.trim();
-    const hostName = document.querySelector('#inputHostName').value.trim();
-    const eventType = document.querySelector('#inputEventType').value.trim();
-
-    // let eventName = $('#inputEventName').val();
-    let hostName = $('#inputHostName').val();
-    let eventDate = $('#eventDate').val();
-    let eventType = $('#inputEventType').val();
-    let eventDescr = $('#eventDescription').val();
-    let localeName = $('#inputLocationName').val();
-    let address = $('#inputStreetAddress').val();
-    let eventCity = $('#inputCity').val();
-    let eventState = $('#inputState').val();
-    let eventZip = $('#inputZip').val();
-    let inviteEmails = $('#inputEmails').val();
-
-    
-   
     function createFormHandle (event) {
         event.preventDefault();
+        const eventName = document.querySelector('#inputEventName').value.trim();
+        const hostName = document.querySelector('#inputHostName').value.trim();
+        const eventType = document.querySelector('#inputEventType').value;
+        const eventDate = document.querySelector('#eventDate').value;
+        const eventDescr = document.querySelector('#eventDescription').value.trim();
+        const address = document.querySelector('#inputStreetAddress').value.trim().toUpperCase();
+        const localeName = document.querySelector('#inputLocationName').value.trim();
+        const eventCity = document.querySelector('#inputCity').value.trim();
+        const eventState = document.querySelector('#inputState').value;
+        const eventZip = document.querySelector('#inputzip').value.trim();
+        const inviteEmails = document.querySelector('#inputEmails').value;
+        
 
-        console.log(eventName, hostName, eventType);
+
+
+        console.log(eventName, hostName, eventType, eventDate, eventDescr, address, localeName, eventCity,eventState, eventZip, inviteEmails);
         // const response = JSON.stringify({eventCity, eventDate, eventDescr})
         // localStorage.saveItem("response", response)
         // const response = await fetch('/api/event', {
